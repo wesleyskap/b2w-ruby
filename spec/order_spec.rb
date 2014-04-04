c278d6ab3fb2bea45f7b8008fa81600a7ecb5db8
Diego Carrion <dc.rec1@gmail.com>
2014-04-03 22:26:39 -0300

    use cassets in specs

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe B2W::Order do
   describe ".all" do
     it "returns the orders details" do
-      B2W::Order.all.first["id"].should == "65"
+      VCR.use_cassette('orders') do
+        B2W::Order.all.first["customer"]['pf']['cpf'].should == "11111111111"
+      end
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,15 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'webmock'
+require 'vcr'
 require 'b2w'
 
-RSpec.configure do |config|
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+end
 
+RSpec.configure do |config|
 end
 
 B2W.config! token: File.read('spec/token').strip, sandbox: true
