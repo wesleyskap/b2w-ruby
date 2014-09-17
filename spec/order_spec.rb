daf098e0664261da3d6ed26637c323643781d33a
Diego Carrion <dc.rec1@gmail.com>
2014-09-17 15:01:07 -0300

    removed deprecations

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,23 +4,23 @@ describe B2W::Order do
   describe ".all" do
     it "returns the orders details" do
       VCR.use_cassette('orders') do
-        B2W::Order.all.first["customer"]['pf']['cpf'].should == "11111111111"
+        expect(B2W::Order.all.first["customer"]['pf']['cpf']).to eql "11111111111"
       end
     end
   end
 
   describe ".approved" do
     it "returns the approved orders details" do
-      RestClient::Request.should_receive(:execute) do |params|
-        params[:url].should include("status=APPROVED")
+      expect(RestClient::Request).to receive(:execute) do |params|
+        expect(params[:url]).to include("status=APPROVED")
         '{"orders": []}'
       end
       B2W::Order.approved
     end
 
     it "accepts the purchase date" do
-      RestClient::Request.should_receive(:execute) do |params|
-        params[:url].should include("purchaseDate=2014-03-12")
+      expect(RestClient::Request).to receive(:execute) do |params|
+        expect(params[:url]).to include("purchaseDate=2014-03-12")
         '{"orders": []}'
       end
       B2W::Order.approved(purchase_date: '2014-03-12')
@@ -30,11 +30,11 @@ describe B2W::Order do
   describe "#processing!" do
     it "should update the order status as processing" do
       orders = VCR.use_cassette('orders') { B2W::Order.all }
-      RestClient::Request.should_receive(:execute) do |params|
-        params[:method].should == :put
-        params[:headers][:content_type].should == 'application/json'
-        params[:payload].should == '{"status":"PROCESSING"}'
-        params[:url].should == "https://api-marketplace.submarino.com.br/v1/order/67/status"
+      expect(RestClient::Request).to receive(:execute) do |params|
+        expect(params[:method]).to eql :put
+        expect(params[:headers][:content_type]).to eql 'application/json'
+        expect(params[:payload]).to eql '{"status":"PROCESSING"}'
+        expect(params[:url]).to eql "https://api-marketplace.submarino.com.br/v1/order/67/status"
         ""
       end
       orders.first.processing!
@@ -44,11 +44,11 @@ describe B2W::Order do
   describe "#processing!" do
     it "should update the order status as processing" do
       orders = VCR.use_cassette('orders') { B2W::Order.all }
-      RestClient::Request.should_receive(:execute) do |params|
-        params[:method].should == :put
-        params[:headers][:content_type].should == 'application/json'
-        params[:payload].should == '{"status":"PROCESSING"}'
-        params[:url].should == "https://api-marketplace.submarino.com.br/v1/order/67/status"
+      expect(RestClient::Request).to receive(:execute) do |params|
+        expect(params[:method]).to eql :put
+        expect(params[:headers][:content_type]).to eql 'application/json'
+        expect(params[:payload]).to eql '{"status":"PROCESSING"}'
+        expect(params[:url]).to eql "https://api-marketplace.submarino.com.br/v1/order/67/status"
         ""
       end
       orders.first.processing!
@@ -58,11 +58,11 @@ describe B2W::Order do
   describe "#invoiced!" do
     it "should update the order status as invoiced" do
       orders = VCR.use_cassette('orders') { B2W::Order.all }
-      RestClient::Request.should_receive(:execute) do |params|
-        params[:method].should == :put
-        params[:headers][:content_type].should == 'application/json'
-        params[:payload].should == "{\"status\":\"PROCESSING\",\"invoiced\":{\"key\":\"123\",\"number\":\"456\",\"line\":\"789\",\"issueDate\":\"2014-01-31\"}}"
-        params[:url].should == "https://api-marketplace.submarino.com.br/v1/order/67/status"
+      expect(RestClient::Request).to receive(:execute) do |params|
+        expect(params[:method]).to eql :put
+        expect(params[:headers][:content_type]).to eql 'application/json'
+        expect(params[:payload]).to eql "{\"status\":\"PROCESSING\",\"invoiced\":{\"key\":\"123\",\"number\":\"456\",\"line\":\"789\",\"issueDate\":\"2014-01-31\"}}"
+        expect(params[:url]).to eql "https://api-marketplace.submarino.com.br/v1/order/67/status"
         ""
       end
       orders.first.invoiced! key: "123", number: "456", line: "789", issueDate: "2014-01-31"
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -6,14 +6,14 @@ describe B2W::Product do
       product = VCR.use_cassette('product_success') do
         B2W::Product.create!(id: 'b2w-ruby-1', name: 'B2W Ruby', sku: [{ id: 'b2w-ruby-1', name: 'B2W Ruby', ean: ['1234567890128'], weight: 1, stockQuantity: 3, enable: true, price: { sellPrice: 10, listPrice: 10 }}], manufacturer: { name: 'Digital Pages', model: 'b2w-ruby', warrantyTime: 1 }, deliveryType: 'SHIPMENT', nbm: { number: '49019900', origin: '1' })
       end
-      product.should be_persisted
+      expect(product).to be_persisted
     end
 
     it "should not persist an invalid product" do
       product = VCR.use_cassette('product_not_success') do
         B2W::Product.create!(id: 'b2w-ruby-2', name: 'B2W Ruby', sku: [{ id: 'b2w-ruby-2', name: 'B2W Ruby', ean: ['invalid'], weight: 1, stockQuantity: 3, enable: true, price: { sellPrice: 10, listPrice: 10 }}], manufacturer: { name: 'Digital Pages', model: 'b2w-ruby', warrantyTime: 1 }, deliveryType: 'SHIPMENT', nbm: { number: '49019900', origin: '1' })
       end
-      product.should_not be_persisted
+      expect(product).to_not be_persisted
     end
   end
 
@@ -21,25 +21,25 @@ describe B2W::Product do
     it "should update a valid product" do
       product = B2W::Product.new 'sku' => 'b2w-ruby-1'
       VCR.use_cassette('product_update_true') do
-        product.update!(id: 'b2w-ruby-1', name: 'B2W Ruby', ean: ['1234567890128'], weight: 1, stockQuantity: 3, enable: true, price: { sellPrice: 10, listPrice: 10 }).should be_true
+        expect(product.update!(id: 'b2w-ruby-1', name: 'B2W Ruby', ean: ['1234567890128'], weight: 1, stockQuantity: 3, enable: true, price: { sellPrice: 10, listPrice: 10 })).to be true
       end
     end
 
     it "should not update an invalid product" do
       product = B2W::Product.new 'sku' => 'invalid'
       VCR.use_cassette('product_update_false') do
-        product.update!(id: 'b2w-ruby-1', name: 'B2W Ruby', ean: ['1234567890128'], weight: 1, stockQuantity: 3, enable: true, price: { sellPrice: 10, listPrice: 10 }).should be_false
+        expect(product.update!(id: 'b2w-ruby-1', name: 'B2W Ruby', ean: ['1234567890128'], weight: 1, stockQuantity: 3, enable: true, price: { sellPrice: 10, listPrice: 10 })).to be false
       end
     end
   end
 
   describe "#update_price!" do
     it "should update the prices" do
-      RestClient::Request.should_receive(:execute) do |params|
-        params[:method].should == :put
-        params[:headers][:content_type].should == 'application/json'
-        params[:payload].should == '{"sellPrice":11.14,"listPrice":12.34}'
-        params[:url].should == "https://api-marketplace.submarino.com.br/v1/sku/b2w-ruby-1/price"
+      expect(RestClient::Request).to receive(:execute) do |params|
+        expect(params[:method]).to eql :put
+        expect(params[:headers][:content_type]).to eql 'application/json'
+        expect(params[:payload]).to eql '{"sellPrice":11.14,"listPrice":12.34}'
+        expect(params[:url]).to eql "https://api-marketplace.submarino.com.br/v1/sku/b2w-ruby-1/price"
         ""
       end
       B2W::Product.new('sku' => 'b2w-ruby-1', 'sell_price' => 11.14, 'list_price' => 12.34).update_price!
@@ -48,11 +48,11 @@ describe B2W::Product do
 
   describe "#update_stock!" do
     it "should update the stock" do
-      RestClient::Request.should_receive(:execute) do |params|
-        params[:method].should == :put
-        params[:headers][:content_type].should == 'application/json'
-        params[:payload].should == '{"quantity":2}'
-        params[:url].should == "https://api-marketplace.submarino.com.br/v1/sku/b2w-ruby-1/stock"
+      expect(RestClient::Request).to receive(:execute) do |params|
+        expect(params[:method]).to eql :put
+        expect(params[:headers][:content_type]).to eql 'application/json'
+        expect(params[:payload]).to eql '{"quantity":2}'
+        expect(params[:url]).to eql "https://api-marketplace.submarino.com.br/v1/sku/b2w-ruby-1/stock"
         ""
       end
       B2W::Product.new('sku' => 'b2w-ruby-1', 'quantity' => 2).update_stock!
@@ -62,13 +62,13 @@ describe B2W::Product do
   describe "#exists?" do
     it "should return true if the sku exists" do
       VCR.use_cassette('product_exists_true') do
-        B2W::Product.new('sku' => 'b2w-ruby-1').exists?.should be_true
+        expect(B2W::Product.new('sku' => 'b2w-ruby-1').exists?).to be true
       end
     end
 
     it "should return false if the sku doesnt exists" do
       VCR.use_cassette('product_exists_false') do
-        B2W::Product.new('sku' => '0123').exists?.should be_false
+        expect(B2W::Product.new('sku' => '0123').exists?).to be false
       end
     end
   end
